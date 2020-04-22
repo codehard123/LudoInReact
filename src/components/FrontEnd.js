e6cc8abba7ad0a54950fd829adfceb6dbb33760a
Samarth Goel <goel.samarth18@gmail.com>
2020-04-22 19:10:15 +0530

    First Commit

--- src/components/FrontEnd.js
+++ src/components/FrontEnd.js
@@ -1,421 +1,13 @@
-import React from "react";
-import FrontEnd from "./components/FrontEnd"
-import Logo from "./Capture.PNG";
+import React from "react"
+import Circle from "./circle"
+import Box from "./Box"
+import Button from "./Button"
+import Logo from "../Capture.PNG"
 
-import Button from "./components/Button";
-import Box from "./components/Box";
-import Circle from "./components/circle"
-var num = null;
-var element;
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      pawn: [
-        {
-          name: 0,
-          color: "green",
-          top: 50,
-          left: 40,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:50,
-          startLeft:40
-        },
-        {
-          name: 1,
-          color: "green",
-          top: 50,
-          left: 140,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:50,
-          startLeft:140
-        },
-        {
-          name: 2,
-          color: "green",
-          top: 150,
-          left: 40,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:150,
-          startLeft:40
-        },
-        {
-          name: 3,
-          color: "green",
-          top: 150,
-          left: 140,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:150,
-          startLeft:140
-        },
-        {
-          name: 4,
-          color: "yellow",
-          top: 50,
-          left: 440,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:50,
-          startLeft:440
-        },
-        {
-          name: 5,
-          color: "yellow",
-          top: 50,
-          left: 550,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:50,
-          startLeft:550
-        },
-        {
-          name: 6,
-          color: "yellow",
-          top: 150,
-          left: 440,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:150,
-          startLeft:440
-        },
-        {
-          name: 7,
-          color: "yellow",
-          top: 150,
-          left: 550,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:150,
-          startLeft:550
-        },
-        {
-          name: 8,
-          color: "red",
-          top: 450,
-          left: 40,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-
-          startTop:450,
-          startLeft:40
-        },
-        {
-          name: 9,
-          color: "red",
-          top: 450,
-          left: 140,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:450,
-          startLeft:140
-        },
-        {
-          name: 10,
-          color: "red",
-          top: 550,
-          left: 40,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:550,
-          startLeft:40
-        },
-        {
-          name: 11,
-          color: "red",
-          top: 550,
-          left: 140,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:550,
-          startLeft:140
-        },
-        {
-          name: 12,
-          color: "blue",
-          top: 450,
-          left: 440,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:450,
-          startLeft:440
-        },
-        {
-          name: 13,
-          color: "blue",
-          top: 450,
-          left: 550,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:450,
-          startLeft:550
-        },
-        {
-          name: 14,
-          color: "blue",
-          top: 550,
-          left: 440,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:550,
-          startLeft:440
-        },
-        {
-          name: 15,
-          color: "blue",
-          top: 550,
-          left: 550,
-          isOut: false,
-          inHouse: true,
-          onStar: false,
-          startTop:550,
-          startLeft:550
-        }
-      ],
-      pasa: null,
-      PlayerArray: ["red", "green", "yellow", "blue"],
-      index: 0,
-      winner: null
-    }
-  }
-
-  Dice = event => {
-    num = Math.floor(Math.random()*6)+1
-    this.setState({
-      pasa: num
-    });
-    element = event.target; //Dice is disabled
-    element.disabled = true;
-
-  }
-  movepaw = item => {
-
-
-    var currentPlayer = this.state.PlayerArray[this.state.index];
-    var pitna=0;
-    var name = item.name;
-    var color = item.color;
-    var pasa = this.state.pasa;
-    var inHouse = item.inHouse;
-    var mappedTop, mappedLeft;
-    var left = item.left;
-    var top = item.top;
-    if (color == "red") {
-      mappedTop = 550;
-      mappedLeft = 250;
-    } else if (color == "green") {
-      mappedTop = 250;
-      mappedLeft = 50;
-    } else if (color == "yellow") {
-      mappedTop = 50;
-      mappedLeft = 350;
-    } else if (color == "blue") {
-      mappedTop = 350;
-      mappedLeft = 550;
-    }
-    var pawnNew;
-    //For movement
-    var newX, newY;
-    if (currentPlayer == color) {
-console.log("StartLeft is",this.state.pawn[name].startLeft);
-
-      //Selected pawn belongs to correct player
-      if ((pasa == 6 && inHouse)||(pasa==1 && inHouse)) {
-      	element.disabled=false;
-        newX = mappedLeft;
-        newY = mappedTop;
-         pawnNew = this.state.pawn;
-      pawnNew[name] = {
-        name: name,
-        color: color,
-        left: newX,
-        top: newY,
-        isOut: false,
-        inHouse: false,   
-        onStar: true,
-        startLeft:this.state.pawn[name].startLeft,
-        startTop:this.state.pawn[name].startTop
-      };
-
-      this.setState({
-        pawn: pawnNew,
-
-      });
-
-      } else if (!inHouse) {
-
-
-        var box_grid = document.getElementById("grid");
-        var pasanum=this.state.pasa;
-        while(pasanum--)
-        {
-          
-        var leftpx=left.toString();
-        leftpx=leftpx+"px";
-        var toppx=top.toString();
-        toppx=toppx+"px";
-        var dir;
-        for (var i = 0; i < box_grid.children.length; i++) {
-        	if (
-            box_grid.children[i].style.left == leftpx &&
-            box_grid.children[i].style.top == toppx
-          ) {
-        		dir=box_grid.children[i].getAttribute("dir");
-            var type=box_grid.children[i].getAttribute("type");
-
-            if(type=="NS")
-            {
-
-               var turn=box_grid.children[i].getAttribute("turn");
-               console.log("Turn is",turn);
-            }
-
-
-            break;
-          }
-        }
-        if(turn=="red" && color=="red")
-        {
-          dir=0;
-        }
-        if(turn=="green" && color=="green")
-        {
-          dir=2;
-        }
-        if(turn=="yellow" && color=="yellow")
-        {
-          dir=4;
-        }
-        if(turn=="blue" && color=="blue")
-        {
-          dir=6;
-        }
-       	if (dir == 0) {
-          newX = left;
-          newY = top - 50;
-        }
-        if (dir == 1) {
-          newX = left + 50;
-          newY = top - 50;
-        }
-        if (dir == 2) {
-          newX = left + 50;
-          newY = top;
-        }
-        if (dir == 3) {
-          newX = left + 50;
-          newY = top + 50;
-        }
-        if (dir == 4) {
-          newX = left;
-          newY = top + 50;
-        }
-        if (dir == 5) {
-          newX = left - 50;
-          newY = top + 50;
-        }
-        if (dir == 6) {
-          newX = left - 50;
-          newY = top;
-        }
-        if (dir == 7) {
-          newX = left - 50;
-          newY = top - 50;
-        }
-     	 pawnNew = this.state.pawn;
-      pawnNew[name] = {
-        name: name,
-        color: color,
-        left: newX,
-        top: newY,
-        isOut: false,
-        inHouse: false,
-        onStar: true,
-        startLeft:this.state.pawn[name].startLeft,
-        startTop:this.state.pawn[name].startTop
-      };
-
-      this.setState({
-        pawn: pawnNew,
-
-      });
-    left=newX;
-    top=newY;
-
- }
-
-
-        //console.log(box_grid.children[0].l0eft,left);
-         
-        for(var i=0;i<this.state.pawn.length;i++)
-        {//Checking goti ka pitna
-
-          if(this.state.pawn[i].left==left && this.state.pawn[i].top==top && this.state.pawn[i].color!=color)
-          {
-            //this.state.pawn[i] pit gayi
-            pitna=1;
-            var copy=this.state.pawn;
-            copy[i]={name: name,
-        color: this.state.pawn[i].color,
-        left: this.state.pawn[i].startLeft,
-        top: this.state.pawn[i].startTop,
-        isOut: false,
-        inHouse: true,
-        onStar: true,
-        startLeft:this.state.pawn[i].startLeft,
-        startTop:this.state.pawn[i].startTop
-
-            }
-            this.setState({pawn:copy});
-            break;
-          }
-
-        }
-        
-         }
-         element.disabled=false;
-         if(this.state.pasa!=6 && pitna==0)
-         {
-         this.setState(prevState => {
-            return {
-                index: (prevState.index + 1)%4
-            }
-        })
-       }
-        currentPlayer=this.state.PlayerArray[this.state.index];
-        
-
-  }
-
-}
-
-
-
-
-
-
-  render(){
-  return (
-        <div><Circle top="50px" left="40px" />
+const FrontEnd=()=>{
+	return (
+		<div>
+        <Circle top="50px" left="40px" />
         <Circle top="50px" left="140px" />
         <Circle top="150px" left="40px" />
         <Circle top="150px" left="140px" />
@@ -506,7 +98,6 @@ console.log("StartLeft is",this.state.pawn[name].startLeft);
             color="white"
             top="0px"
             left="300px"
-            turn="yellow"
           />
           <Box
             height="50px"
@@ -593,7 +184,7 @@ console.log("StartLeft is",this.state.pawn[name].startLeft);
           <Box
             height="50px"
             dir="7"
-
+            
             width="50px"
             type="N"
             color="white"
@@ -680,7 +271,6 @@ console.log("StartLeft is",this.state.pawn[name].startLeft);
             color="white"
             top="600px"
             left="300px"
-            turn="red"
           />
           <Box
             height="50px"
@@ -781,7 +371,6 @@ console.log("StartLeft is",this.state.pawn[name].startLeft);
             color="white"
             top="300px"
             left="0px"
-            turn="green"
           />
           <Box
             height="50px"
@@ -792,7 +381,15 @@ console.log("StartLeft is",this.state.pawn[name].startLeft);
             top="300px"
             left="50px"
           />
-          <Box  height="50px" dir="2" type="N" width="50px" color="green" top="300px" left="100px"/>
+          <Box
+            height="50px"
+            dir="2"
+            type="N"
+            width="50px"
+            color="green"
+            top="300px"
+            left="100px"
+          />
           <Box
             height="50px"
             dir="2"
@@ -946,7 +543,6 @@ console.log("StartLeft is",this.state.pawn[name].startLeft);
             color="white"
             top="300px"
             left="600px"
-            turn="blue"
           />
           <Box
             height="50px"
@@ -995,7 +591,7 @@ console.log("StartLeft is",this.state.pawn[name].startLeft);
           />
         </div>
 
-
+        
         {this.state.pawn.map((item, index) => {
           return (
             <div onClick={this.movepaw.bind(this, item)}>
@@ -1013,18 +609,13 @@ console.log("StartLeft is",this.state.pawn[name].startLeft);
             top: "300px",
             left: "900px"
           }}
-          >
+        >
           {this.state.pasa}
         </button>
         <img src={Logo} className="Logo" />
-     
-         </div>
-         )
+      </div>  
+    );
 
-  }
-
-
-
-
+		
 }
-export default App;
+export default FrontEnd
